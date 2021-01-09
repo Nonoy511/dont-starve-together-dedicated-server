5f53ec1f799df15ee08954f8ba61f85ed3d0cfaf
Nonoy Suguitan <nonoy.suguitan@gmail.com>
2021-01-08 16:01:07 -0800

    testing adding mods

---
+++ dedicated_server_mods_setup.lua
@@ -0,0 +1,26 @@
+--There are two functions that will install mods, ServerModSetup and ServerModCollectionSetup. Put the calls to the functions in this file and they will be executed on boot.
+
+--ServerModSetup takes a string of a specific mod's Workshop id. It will download and install the mod to your mod directory on boot.
+	--The Workshop id can be found at the end of the url to the mod's Workshop page.
+	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=350811795
+	--ServerModSetup("350811795")
+
+--ServerModCollectionSetup takes a string of a specific mod's Workshop id. It will download all the mods in the collection and install them to the mod directory on boot.
+	--The Workshop id can be found at the end of the url to the collection's Workshop page.
+	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
+	--ServerModCollectionSetup("379114180")
+
+--#"Display food values"
+ServerModSetup("347079953")
+
+--#"Increased Stack size"
+ServerModSetup("374550642")
+
+--#"Health Info"
+ServerModSetup("375859599")
+
+--#"Global Positions"
+ServerModSetup("378160973")
+
+--#"Campfire Respawn"
+ServerModSetup("569043634")
